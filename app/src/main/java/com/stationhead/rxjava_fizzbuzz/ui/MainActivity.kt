c743fd3f3633bc83d07b5b5487eeae640440fe83
Andrew DeFranco <andrew@stationhead.com>
2021-03-08 19:38:58 -0500

    phase-2

--- app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivity.kt
+++ app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivity.kt
@@ -4,16 +4,30 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.stationhead.rxjava_fizzbuzz.R
+import com.trello.rxlifecycle2.android.lifecycle.kotlin.bindToLifecycle
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private val model: MainActivityViewModel by lazy { ViewModelProvider(this).get(
         MainActivityViewModel::class.java) }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        model.ticker
+            .observeOn(AndroidSchedulers.mainThread())
+            .bindToLifecycle(this)
+            .subscribe {
+                when(it) {
+                    MainActivityViewModel.FlashState.ON -> amberLight.isActivated = true
+                    MainActivityViewModel.FlashState.OFF -> amberLight.isActivated = false
+                }
+            }
     }
+
 }

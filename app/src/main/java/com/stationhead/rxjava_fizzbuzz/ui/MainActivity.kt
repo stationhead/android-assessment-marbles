001dc1d0fa43b4dfbf28e319fc4376f07c62175b
Andrew DeFranco <andrew@stationhead.com>
2021-03-08 22:02:25 -0500

    phase-3

--- app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivity.kt
+++ app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivity.kt
@@ -14,6 +14,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupPickers()
+    }
 
+    private fun setupPickers() {
+        leftPicker.minValue = 3
+        leftPicker.maxValue = 9
+        leftPicker.wrapSelectorWheel = false
+
+        rightPicker.minValue = 5
+        rightPicker.maxValue = 13
+        rightPicker.wrapSelectorWheel = false
+
+        leftPicker.setOnValueChangedListener { _, _, newVal ->
+            model.leftPickerValueUpdated(newVal)
+        }
+        rightPicker.setOnValueChangedListener { _, _, newVal ->
+            model.rightPickerValueUpdated(newVal)
+        }
     }
 }
--- app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivityViewModel.kt
@@ -3,15 +3,22 @@ package com.stationhead.rxjava_fizzbuzz.ui
 import androidx.lifecycle.ViewModel
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.BehaviorSubject
 import java.util.concurrent.TimeUnit
 
 class MainActivityViewModel : ViewModel() {
+
+    val fizzCount = BehaviorSubject.create<Int>()
+    val buzzCount = BehaviorSubject.create<Int>()
 
     public override fun onCleared() {
         super.onCleared()
         disposables.clear()
     }
 
+    val leftPickerValueUpdated = fizzCount::onNext
+
+    val rightPickerValueUpdated  = buzzCount::onNext
     private val disposables by lazy { CompositeDisposable() }
 
     val ticker: Observable<Unit> = Observable.interval(1, TimeUnit.SECONDS).map { _ -> Unit }

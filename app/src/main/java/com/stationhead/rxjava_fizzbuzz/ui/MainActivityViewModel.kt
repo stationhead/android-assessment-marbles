b71ec8850ee87c38870e529571bbd02385d05f94
Andrew DeFranco <andrew@stationhead.com>
2021-03-31 18:57:09 -0400

    use tickprovider

--- app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivityViewModel.kt
@@ -1,6 +1,7 @@
 package com.stationhead.rxjava_fizzbuzz.ui
 
 import androidx.lifecycle.ViewModel
+import com.stationhead.rxjava_fizzbuzz.usecase.TickProvider
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import java.util.concurrent.TimeUnit
@@ -14,7 +15,7 @@ class MainActivityViewModel : ViewModel() {
 
     private val disposables by lazy { CompositeDisposable() }
 
-    val ticker: Observable<FlashState> = Observable.interval(1, TimeUnit.SECONDS).flash()
+    val ticker = TickProvider().getTicks().flash()
 
     private fun Observable<*>.flash() = flatMap { _ ->
         Observable.just(FlashState.ON)

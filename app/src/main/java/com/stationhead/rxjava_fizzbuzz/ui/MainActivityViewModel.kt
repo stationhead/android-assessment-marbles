616cca6ee3068373528360d1f560350c7ebc9681
Andrew DeFranco <andrew@stationhead.com>
2021-03-31 18:58:32 -0400

    tickprovider

--- app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivityViewModel.kt
@@ -1,6 +1,7 @@
 package com.stationhead.rxjava_fizzbuzz.ui
 
 import androidx.lifecycle.ViewModel
+import com.stationhead.rxjava_fizzbuzz.usecase.TickProvider
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import java.util.concurrent.TimeUnit
@@ -14,7 +15,7 @@ class MainActivityViewModel : ViewModel() {
 
     private val disposables by lazy { CompositeDisposable() }
 
-    val ticker: Observable<Unit> = Observable.interval(1, TimeUnit.SECONDS).map { _ -> Unit }
+    val ticker: Observable<Unit> = TickProvider().getTicks()
 
 
 }

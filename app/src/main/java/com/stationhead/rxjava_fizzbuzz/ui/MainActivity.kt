fe30e419feac6b324df5c0f468470a41ae2dd4e4
Andrew DeFranco <azarkon@gmail.com>
2021-04-08 11:18:32 -0400

    Phase 3 (#1)
    * phase-2
    
    * phase-3
    
    * phase 2 objective added.
    
    * add phase-3 instructions
    
    * use tickprovider

--- app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivity.kt
+++ app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.stationhead.rxjava_fizzbuzz.R
+import com.trello.rxlifecycle2.android.lifecycle.kotlin.bindToLifecycle
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +19,38 @@ class MainActivity : AppCompatActivity() {
     // Green light should turn on every 5 seconds
     // Lights will turn when `isActivated` is true
 
+    // Phase-2 objective:
+    // integrate flash functionality.
+    // update status label to read:
+    //    "Fizz" when the amber light flashes,
+    //    "Buzz" when the green light flashes,
+    //    "FizzBuzz" when both lights flashes, and
+    //    nothing ("") when neither light flashes.
+
+    //Phase-3 objective:
+    // Integrate Spinners to let user select rate of flashes.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setupPickers()
     }
+
+    private fun setupPickers() {
+        leftPicker.minValue = 3
+        leftPicker.maxValue = 9
+        leftPicker.wrapSelectorWheel = false
+
+        rightPicker.minValue = 5
+        rightPicker.maxValue = 13
+        rightPicker.wrapSelectorWheel = false
+
+        leftPicker.setOnValueChangedListener { _, _, newVal ->
+            model.leftPickerValueUpdated(newVal)
+        }
+        rightPicker.setOnValueChangedListener { _, _, newVal ->
+            model.rightPickerValueUpdated(newVal)
+        }
+    }
+
 }
--- app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/stationhead/rxjava_fizzbuzz/ui/MainActivityViewModel.kt
@@ -4,18 +4,24 @@ import androidx.lifecycle.ViewModel
 import com.stationhead.rxjava_fizzbuzz.usecase.TickProvider
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.BehaviorSubject
 import java.util.concurrent.TimeUnit
 
 class MainActivityViewModel : ViewModel() {
+
+    val fizzCount = BehaviorSubject.create<Int>()
+    val buzzCount = BehaviorSubject.create<Int>()
 
     public override fun onCleared() {
         super.onCleared()
         disposables.clear()
     }
 
+    val leftPickerValueUpdated = fizzCount::onNext
+
+    val rightPickerValueUpdated  = buzzCount::onNext
+
     private val disposables by lazy { CompositeDisposable() }
 
-    val ticker: Observable<Unit> = TickProvider().getTicks()
-
-
+    val ticker = TickProvider().getTicks()
 }
